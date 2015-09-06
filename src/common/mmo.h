91a34b5e0cda9e5ef783f17c0f114c5efbca0ee0
Haru <haru@dotalux.com>
2015-09-06 18:08:14 +0200

    Ported char-server.conf to libconfig
    Fixed issue: 8115, now start items can be equipped as well
    
    Ported to modern Hercules and cleaned up from Panikon's commits: c6482e9870645ffe59a6a059b819574d4ac79fd9, 832fb27d4f767e4bc8b68c432d0da00b7cb7a4f9, f81b579899e3a15bd472ca8c6a6e0116c43bec92, e23723725499b617def03d05661eca637edaeabd, 0b783a83d82e588efd760f7f4baec0c8074a6fd1, 1b7de91308a57ea07b158ed95a2515a3c8cc36bd, 677d3430cbda0962b320a60cf499e9dadf637d00
    
    Signed-off-by: Haru <haru@dotalux.com>

--- src/common/mmo.h
+++ src/common/mmo.h
@@ -147,7 +147,6 @@
 #define MAX_GUILDLEVEL 50
 #define MAX_GUARDIANS 8                  // Local max per castle. [Skotlex]
 #define MAX_QUEST_OBJECTIVES 3           // Max quest objectives for a quest
-#define MAX_START_ITEMS 32               // Max number of items allowed to be given to a char whenever it's created. [mkbu95]
 
 // for produce
 #define MIN_ATTRIBUTE 0
@@ -297,7 +296,7 @@ enum equip_pos {
 
 struct point {
 	unsigned short map;
-	short x,y;
+	int16 x, y;
 };
 
 enum e_skill_flag
@@ -995,6 +994,7 @@ enum e_char_server_type {
 	CST_OVER18      = 2,
 	CST_PAYING      = 3,
 	CST_F2P         = 4,
+	CST_MAX,
 };
 
 enum e_pc_reg_loading {
